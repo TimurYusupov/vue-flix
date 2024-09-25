5b4f959a89e758a503623f56e23983e73866eddc
TimurYusupov <timurzek95@gmail.com>
2024-09-25 19:17:55 +0200

    deleted sidebar paddings and added hover effect

--- src/components/Sidebars/SoonSidebar.vue
+++ src/components/Sidebars/SoonSidebar.vue
@@ -5,29 +5,27 @@ defineProps<{
 </script>
 
 <template>
-   <aside class="h-fit px-2 py-3 rounded-xl" :class="bgColor">
+   <aside class="h-fit rounded-xl" :class="bgColor">
       <div class="bg-custom-light-gray p-2 rounded-tl-lg rounded-tr-lg">
          <h3 class="text-center text-md text-sidebar-title text-shadow-custom font-bold">
             Coming Soon
          </h3>
       </div>
-      <div class="bg-blue-200 py-4 flex flex-col gap-2 items-center">
-         <div class="movie-card relative">
-            <img src="https://via.placeholder.com/140x200" alt="Movie Image" />
+      <div class="bg-blue-200 py-4 flex flex-col gap-3 items-center">
+         <div class="movie-card relative cursor-pointer hover:scale-105 transition-all">
+            <img src="https://via.placeholder.com/150x200" alt="Movie Image" />
             <p class="absolute left-3 bottom-3 text-center text-white text-sm">
                Last Samurai (2014)
             </p>
          </div>
-         <div class="movie-card relative">
-            <img src="https://via.placeholder.com/140x200" alt="Movie Image" />
-            <p class="absolute left-3 bottom-3 text-center text-white text-sm">
-               Last Samurai (2014)
-            </p>
+         <div class="movie-card relative cursor-pointer hover:scale-105 transition-all">
+            <img src="https://via.placeholder.com/150x200" alt="Movie Image" />
+            <p class="absolute left-3 bottom-3 text-center text-white text-sm">Last Ninja (2014)</p>
          </div>
-         <div class="movie-card relative">
-            <img src="https://via.placeholder.com/140x200" alt="Movie Image" />
-            <p class="absolute left-3 bottom-3 text-center text-white text-sm">
-               Last Samurai (2014)
+         <div class="movie-card relative cursor-pointer hover:scale-105 transition-all">
+            <img src="https://via.placeholder.com/150x200" alt="Movie Image" />
+            <p class="absolute left-3 bottom-3 text-center text-white text-sm font-medium">
+               Last Knight (2014)
             </p>
          </div>
       </div>
