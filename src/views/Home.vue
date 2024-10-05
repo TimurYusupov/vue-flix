8ee632d7330fc6a6e686accadc93f58549138e22
TimurYusupov <timurzek95@gmail.com>
2024-10-05 15:19:28 +0200

    create movie card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-// import { inject } from 'vue'
-
-// const inputValue = inject<string>('inputValue')
+import Movie from '@/components/Movie.vue'
 </script>
 
 <template>
    <h1 class="text-center text-2xl font-bold">Watch movies anytime, anywhere</h1>
+
+   <div class="grid gap-4">
+      <Movie />
+      <Movie />
+      <Movie />
+   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ export default {
       colors: {
         'custom-gray': '#282828',
         'custom-light-gray': '#666',
+        'custom-dark-gray': '#2c2c2c',
         'cold-steel': '#4F4F4F',
         'sidebar-title': '#ffd200',
         'sidebar-border': '#454545'
